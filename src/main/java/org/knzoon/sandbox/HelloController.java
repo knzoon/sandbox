f396945b170008e8aacbd49654631df16537caff
Olof Appelblad <117285847+knzoon@users.noreply.github.com>
2025-11-25 13:14:22 +0100

    Adding code

---
+++ src/main/java/org/knzoon/sandbox/HelloController.java
@@ -0,0 +1,13 @@
+package org.knzoon.sandbox;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello world";
+    }
+}
